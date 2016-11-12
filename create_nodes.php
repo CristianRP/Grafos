393da7ab4a029fed6ed3e8a0814f0ff59d975cee
Cristian Ramírez <cristianramirezgt@gmail.com>
2016-11-12 05:32:09 -0600

    Calculo del grado de cada vertice y  modificaciones visuales

--- create_nodes.php
+++ create_nodes.php
@@ -4,10 +4,14 @@
 
 	$arrayVertex = getArrayVertex($all_vertex);
 	$arrayEdges = getArrayEdges($all_edges, $all_vertex);
+	$arrayGrades = drawGrades(getArrayGrades($arrayVertex, $arrayEdges));
 
 	$results[0] = $arrayVertex;
 	$results[1] = $arrayEdges;
+	$results[2] = $arrayGrades;
 
+	// echo "<pre>";
+	// print_r($results);
 	echo json_encode($results);
 
 	function getArrayVertex($all_vertex){
@@ -56,4 +60,31 @@
 				return $all_vertex[$i]['id'];
 			}
 		}
+	}
+
+	function getArrayGrades($arrayVertex, $arrayEdges){
+		$all_grades = null;
+
+		foreach ($arrayVertex as $v) {
+			$cantidad = 0;
+			$vertice_id = $v['id'];
+
+			foreach ($arrayEdges as $e) {
+				if($vertice_id == $e['from'] || $vertice_id == $e['to']){
+					$cantidad = $cantidad + 1;
+					$all_grades[$vertice_id] = array('name' => $v['label'], 'id' => $vertice_id, 'grade' => $cantidad);
+				}
+			}
+		}
+
+		return $all_grades;
+	}
+
+	function drawGrades($all_grades){
+		$return = null;
+		foreach ($all_grades as $grade) {
+			$return = $return . '<p><strong>Grado de '.$grade['name'].':</strong> ['.$grade['grade'].']</p><br>';
+		}
+
+		return $return;
 	}
--- index.php
+++ index.php
@@ -24,43 +24,90 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Matemática Discreta</a>
+                    <a class="navbar-brand" href="index.php">Matemática Discreta</a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Grafos <span class="sr-only">(current)</span></a></li>
-                        <li><a href="euclides.php">Algoritmo de Euclides</a></li>
+                        <li class="active"><a href="#" class="graphs">Grafos <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#" class="euclides">Algoritmo de Euclides</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
 
         <div class="panel">
-            <div class="col-md-6 well bs-component" id="mynetwork"></div>
+            <div class="graphs-form">
+                <div class="col-md-6 well bs-component" id="mynetwork"></div>
 
-            <form class="form-horizontal col-md-6 well bs-component" style="margin: 20px;" method="POST" action="create_nodes.php" id="create_nodes">
-                <fieldset>
-                    <legend>Grafos</legend>
-                    <div class="form-group">
-                        <label for="inputEmail" class="col-lg-2 control-label">Vértices:</label>
-                        <div class="col-lg-10">
-                            <input type="text" class="form-control" name="vertices" id="vertices" value="{v1, v2, v3, v4, v5, v6, v7}" style="width: 450px;">
+                <form class="form-horizontal col-md-6 well bs-component" style="margin: 20px;" method="POST" action="create_nodes.php" id="create_nodes">
+                    <fieldset>
+                        <legend>Grafos</legend>
+                        <div class="form-group">
+                            <label for="inputEmail" class="col-lg-2 control-label">Vértices:</label>
+                            <div class="col-lg-10">
+                                <input type="text" class="form-control" name="vertices" id="vertices" value="{v1, v2, v3, v4, v5, v6, v7}" style="width: 450px;">
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="inputPassword" class="col-lg-2 control-label">Aristas:</label>
-                        <div class="col-lg-10">
-                            <input type="text" class="form-control" id="aristas" name="aristas" value="{(v1,v2),(v1,v7),(v2,v3),(v2,v4),(v3,v7),(v3,v4),(v3,v6),(v4,v5),(v5,v6)}" style="width: 450px;"><br>
+                        <div class="form-group">
+                            <label for="inputPassword" class="col-lg-2 control-label">Aristas:</label>
+                            <div class="col-lg-10">
+                                <input type="text" class="form-control" id="aristas" name="aristas" value="{(v1,v2),(v1,v7),(v2,v3),(v2,v4),(v3,v7),(v3,v4),(v3,v6),(v4,v5),(v5,v6)}" style="width: 450px;"><br>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-lg-10 col-lg-offset-2">
-                            <button type="submit" class="btn btn-primary" id="btnActualizar">Actualizar Grafo</button>
+                        <div class="form-group">
+                            <div class="col-lg-10 col-lg-offset-2">
+                                <button type="submit" class="btn btn-primary" id="btnActualizar">Actualizar Grafo</button>
+                            </div>
                         </div>
-                    </div>
-                </fieldset>
-            </form>
+                    </fieldset>
+                    <fieldset class="preguntas-container" style="display:none;">
+                        <legend>Grados</legend>
+                        <div class="grades-container">
+                            
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+
+            <div class="container col-md-6 euclides-form" style="display:none;">
+                <div class="panel panel-success">
+                  <div class="panel-heading">
+                    <h3 class="panel-title">Algoritmo de Euclides</h3>
+                  </div>
+                  <div class="panel-body">
+                    <form method="POST" action="euclides.php" id="calcular_euclides" class="form-horizontal" target="container">
+                        <fieldset>
+                            <div class="form-group">
+                              <label for="inputEmail" class="col-lg-2 control-label">Primer número</label>
+                              <div class="col-lg-10">
+                                <input type="text" class="form-control" id="num1" name="num1" placeholder="Numero 1">
+                              </div>
+                            </div>
+                            <div class="form-group">
+                              <label for="inputEmail" class="col-lg-2 control-label">Segundo número</label>
+                              <div class="col-lg-10">
+                                <input type="text" class="form-control" id="num2" name="num2" placeholder="Numero 2">
+                              </div>
+                            </div>
+                            <div class="form-group">
+                              <div class="col-lg-10 col-lg-offset-2">
+                                <button type="submit" class="btn btn-primary">Calcular</button>
+                              </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                  </div>
+                </div>
+                <div class="panel panel-success container-results" style="display:none;">
+                  <div class="panel-heading">
+                    <h3 class="panel-title">Resultado</h3>
+                  </div>
+                  <div class="panel-body">
+                    <p class="euclides-results"></p>
+                  </div>
+                </div>
+            </div>
         </div>
 
 <script src="js/jquery.js"></script>
@@ -74,7 +121,10 @@
                 success: function(data) {
                     // console.log(data);
                     var dato = jQuery.parseJSON(data);
-                    console.log(dato);
+                    // console.log(dato);
+                    $('.preguntas-container').show('slow');
+                    $('.grades-container').html(dato[2]);
+                    // getGrades(dato[2]);
                     // create an array with nodes
                     var nodes = new vis.DataSet(dato[0]);
 
@@ -98,6 +148,29 @@
                 }   
             })
             return false;
+        });
+
+        $('#calcular_euclides').submit(function() {
+            $.ajax({
+                type: 'POST',
+                url: $(this).attr('action'),
+                data: $(this).serialize(),
+                success: function(data) {
+                    $('.container-results').show('slow');
+                    $('.euclides-results').html(data);
+                }   
+            })
+            return false;
+        });
+
+        $('.graphs').click(function () {
+            $('.graphs-form').show('slow');
+            $('.euclides-form').hide('slow');
+        });
+
+        $('.euclides').click(function () {
+            $('.euclides-form').show('slow');
+            $('.graphs-form').hide('slow');
         });
     });
 </script>
